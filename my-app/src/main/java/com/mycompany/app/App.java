a1ccf84caf2bc51355eb64d38278606c6faa9cd1
dhruvk-01 <dhruv.kalathia01@gmail.com>
2022-04-10 19:21:25 +0000

    Getting started

---
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,15 @@
+package com.mycompany.app;
+
+import org.mockito.Mockito;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
